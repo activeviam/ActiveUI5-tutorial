0edd4c57eadda04a03c7e6286acd376d94a0a63e
Clement Caylux <cca@activeviam.com>
2022-06-28 21:05:58 +0200

    part-2: Running a query

--- src/custom/Map.tsx
+++ src/custom/Map.tsx
@@ -2,13 +2,71 @@
  * Created by Clement Caylux on 28/06/2022
  */
 
-import {WidgetPluginProps} from "@activeviam/activeui-sdk";
+import {useQueryResult, WidgetPluginProps} from "@activeviam/activeui-sdk";
+import {Spin} from "antd";
 import React, {FC} from "react";
 
 export const Map: FC<WidgetPluginProps> = (props) => {
+
+    const {data, error, isLoading} = useQueryResult({
+        serverKey:"my-server",
+        queryId: "abc",
+        query: {
+            mdx: `SELECT
+           [Countries].[Country].[Country_Name].Members ON ROWS,
+           Crossjoin(
+             [Green-growth].[Year].[Year].Members,
+             [Measures].[Real GDP per capita (USD).MEAN]
+           ) ON COLUMNS
+           FROM [Green-growth]`
+        }
+    })
+
+    console.log(isLoading);
+
+    if (isLoading) {
+        return <Spin/>
+    }
+
+    if (error) {
+        return <div>{error.stackTrace}</div>
+    }
+
+    if (!data) {
+        return null;
+    }
+
+// HERE we use data.axes!
+    const [columnsAxis, rowsAxis] = data.axes;
+    const numberOfColumns = columnsAxis.positions.length;
+
     return (
-        <div style={props.style}>
-            {`placeholder for Map`}
-        </div>
-    )
+        <table>
+            <tr>
+                <th />
+                // The header represents the members of the columns axis (the years).
+                {columnsAxis.positions.map((position, columnIndex) => (
+                    <th key={columnIndex}>{position[0].captionPath[0]}</th>
+                ))}
+            </tr>
+            {rowsAxis.positions.map((position, rowIndex) => {
+                const tableCells: JSX.Element[] = [];
+
+                // the first column represents the members of the rows axis (the countries)
+                tableCells.push(<td key={0}>{position[0].captionPath[2]}</td>);
+
+                for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+                    const cellIndex = rowIndex * numberOfColumns + columnIndex;
+                    // HERE we use data.cells!
+                    const dataCell = data.cells[cellIndex];
+
+                    tableCells.push(
+                        <td key={columnIndex + 1}>{dataCell?.formattedValue}</td>,
+                    );
+                }
+
+                return <tr key={rowIndex}>{tableCells}</tr>;
+            })}
+        </table>
+    );
 }
